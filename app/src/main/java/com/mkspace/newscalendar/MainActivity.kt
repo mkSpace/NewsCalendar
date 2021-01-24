f92d12675d84ce065a88078ed3cdfdf8783d3752
Jaemin.Park <once29@naver.com>
2021-01-24 20:06:30 +0900

    Add funin-base dependency

--- app/src/main/java/com/mkspace/newscalendar/MainActivity.kt
+++ app/src/main/java/com/mkspace/newscalendar/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.mkspace.newscalendar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.funin.base.funinbase.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
